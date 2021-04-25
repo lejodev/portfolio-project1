afe69ec314aac994f090052057be952e4c91d636
lejodev <alejandro.rv97@gmail.com>
2021-04-24 23:28:05 -0500

    services feature added

---
+++ Frontend/src/components/moto-services/MotoServices.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./_moto-services.scss";
+
+const MotoServices = () => {
+  return (
+    <div className="moto-services">
+      <div className="servicesContainer"></div>
+    </div>
+  );
+};
+
+export default MotoServices;
